76a4c3e8329f02aea9558df26a47de398c3807d8
Renzo Maldonado Aguilar <renzoma89@gmail.com>
2016-06-26 11:37:24 -0500

    Db y pojo

--- app/src/main/java/com/android/renzo/facebookrecipes/FacebookRecipesApp.java
+++ app/src/main/java/com/android/renzo/facebookrecipes/FacebookRecipesApp.java
@@ -3,6 +3,7 @@ package com.android.renzo.facebookrecipes;
 import android.app.Application;
 
 import com.facebook.FacebookSdk;
+import com.raizlabs.android.dbflow.config.FlowManager;
 
 /**
  * Created by HOME on 26/06/2016.
@@ -12,12 +13,28 @@ public class FacebookRecipesApp extends Application {
     public void onCreate() {
         super.onCreate();
         initFacebook();
+        initDB();
+    }
+
+    private void initDB() {
+        FlowManager.init(this);
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        DBTearDown();
+    }
+
+    private void DBTearDown() {
+        FlowManager.destroy();
     }
 
     private void initFacebook() {
         FacebookSdk.sdkInitialize(this);
 
     }
+
 
 
 }
---
+++ app/src/main/java/com/android/renzo/facebookrecipes/db/RecipesDatabase.java
@@ -0,0 +1,14 @@
+package com.android.renzo.facebookrecipes.db;
+
+import com.raizlabs.android.dbflow.annotation.Database;
+
+/**
+ * Created by HOME on 26/06/2016.
+ */
+@Database(name = RecipesDatabase.NAME, version = RecipesDatabase.VERSION)
+public class RecipesDatabase {
+    public static final int VERSION = 1;
+    public static final String NAME = "Recipes";
+
+
+}
