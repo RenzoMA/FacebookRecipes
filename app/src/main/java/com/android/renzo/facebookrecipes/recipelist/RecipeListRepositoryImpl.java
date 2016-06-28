c92c64d5e0095c32f66ebdcb6f148812d471997f
Renzo Maldonado Aguilar <renzoma89@gmail.com>
2016-06-27 22:02:03 -0500

    Repository

---
+++ app/src/main/java/com/android/renzo/facebookrecipes/recipelist/RecipeListRepositoryImpl.java
@@ -0,0 +1,49 @@
+package com.android.renzo.facebookrecipes.recipelist;
+
+import com.android.renzo.facebookrecipes.entities.Recipe;
+import com.android.renzo.facebookrecipes.libs.base.EventBus;
+import com.android.renzo.facebookrecipes.recipelist.events.RecipeListEvent;
+import com.raizlabs.android.dbflow.list.FlowCursorList;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by HOME on 27/06/2016.
+ */
+public class RecipeListRepositoryImpl implements RecipeListRepository {
+
+    private EventBus eventBus;
+
+
+
+    @Override
+    public void getSavedRecipes() {
+        FlowCursorList<Recipe> storedRecipes = new FlowCursorList<Recipe>(false, Recipe.class);
+        post(RecipeListEvent.READ_EVENT, storedRecipes.getAll());
+        storedRecipes.close();
+
+    }
+
+    @Override
+    public void updateRecipe(Recipe recipe) {
+        recipe.update();
+        post();
+    }
+
+    @Override
+    public void removeRecipe(Recipe recipe) {
+        recipe.delete();
+        post(RecipeListEvent.DELETE_EVENT, Arrays.asList(recipe));
+    }
+
+    private void post(int type, List<Recipe> recipeList){
+        RecipeListEvent event = new RecipeListEvent();
+        event.setType(type);
+        event.setRecipeList(recipeList);
+        eventBus.post(event);
+    }
+    private void post(){
+        post(RecipeListEvent.UPDATE_EVENT,null);
+    }
+}
