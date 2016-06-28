4ee97afa8a12e00a6527417afeb907d5b268dbd6
Renzo Maldonado Aguilar <renzoma89@gmail.com>
2016-06-27 21:57:10 -0500

    Interactors

---
+++ app/src/main/java/com/android/renzo/facebookrecipes/recipelist/RecipeListInteractorImpl.java
@@ -0,0 +1,17 @@
+package com.android.renzo.facebookrecipes.recipelist;
+
+/**
+ * Created by HOME on 27/06/2016.
+ */
+public class RecipeListInteractorImpl implements RecipeListInteractor {
+    private RecipeListRepository repository;
+
+    public RecipeListInteractorImpl(RecipeListRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void execute() {
+        repository.getSavedRecipes();
+    }
+}
